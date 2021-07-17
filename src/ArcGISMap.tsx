48d67d1bb7a57ee05b160e02516b5cdc0a529ae2
= <olindyla@gmail.com>
2021-07-17 01:40:18 -0700

    added separate points layers, still no popup

--- src/ArcGISMap.tsx
+++ src/ArcGISMap.tsx
@@ -7,8 +7,13 @@ import Point from '@arcgis/core/geometry/Point';
 import Graphic from '@arcgis/core/Graphic';
 import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 import SimpleRenderer from '@arcgis/core/renderers/SimpleRenderer';
+import Query from '@arcgis/core/tasks/support/Query';
+import PopupTemplate from '@arcgis/core/PopupTemplate';
+
+const platingProjectIDfield = "PlantingProjectID"
 
 const ArcGISMap = ()=>{
+
     
   const mapDiv = useRef(null);
 
@@ -30,79 +35,114 @@ const ArcGISMap = ()=>{
       });
 
       const polygonLayer = new FeatureLayer({
-        url:"https://gis-test.arborday.org/server/rest/services/Planting_Project_Location___Polygon/MapServer/0"
+        url:"https://gis-test.arborday.org/server/rest/services/CP_Showcase_MVP_Data_Polygon/FeatureServer/0",
       });
 
-      polygonLayer.queryFeatures({where:"1=1", outFields:['*'], returnGeometry:true }).then(r=>{
-        const extents = []
-        const projectArrays: {[key: string]: Graphic[]} = {};
-        const graphics = r.features.map(f=>{
-          return new Graphic({attributes:f.attributes,geometry:(f.geometry as Polygon).centroid})
-        })
-        graphics.forEach(g=>{
-          projectArrays[g.attributes['PlantingProjectID']] = projectArrays[g.attributes['PlantingProjectID']] || [];
-          projectArrays[g.attributes['PlantingProjectID']].push(g);
-        })
-
-        const zoomedOutPoints:Graphic[] = [];
-
-        Object.entries(projectArrays).forEach(([id,graphics],index)=>{
-          const point = new Graphic({attributes:{'PlantingProjectID':id,'OBJECTID': index}})
-          const x = graphics.reduce((sum,g)=>sum+(g.geometry as Point).x,0)/graphics.length;
-          const y = graphics.reduce((sum,g)=>sum+(g.geometry as Point).y,0)/graphics.length;
-          const spatialReference = graphics[0].geometry.spatialReference;
-          point.geometry = new Point({spatialReference,x,y});
-          zoomedOutPoints.push(point)
-        })
-
-        const pointRenderer = {
-          type:'simple',
-          symbol:{
-            type:'simple-marker',
-            size:8,              
-            outline: { style: "none", width: 0 },
-            color: [0, 196, 0, 1]
-          } 
-        } as unknown as SimpleRenderer;
-
-        const zoomedOutLayer = new FeatureLayer({
-          source:zoomedOutPoints, objectIdField:'OBJECTID',renderer:pointRenderer,
-          maxScale:5000000      
-        });
+      const pointLayer = new FeatureLayer({
+        url:"https://gis-test.arborday.org/server/rest/services/CP_Showcase_MVP_Data_Points/FeatureServer/0",
+      });
 
 
-        const middleLayer = new FeatureLayer({
-          source:graphics, objectIdField:'OBJECTID', renderer:pointRenderer,minScale:4999999,maxScale:100000
-        })
 
-        const polyRenderer = {
-          type:'simple',
-          symbol:{
-          type:'simple-fill',
-          outline: {           color: [0, 196, 0, 1] },
-          color: [0, 196, 0, .25]}
-        } as unknown as SimpleRenderer
-        const polygonLayer = new FeatureLayer({source:r.features,objectIdField:'OBJECTID',renderer:polyRenderer, minScale:99999})
+      const projectQuery = {where:"1=1", outFields:[platingProjectIDfield,'PlantingProjectName','PlantingProjectType','CalendarYear','AcresRestored'], returnGeometry:false, returnDistinctValues:true} as Query;
 
-        map.addMany([zoomedOutLayer,middleLayer,polygonLayer]);
+      Promise.all([polygonLayer.queryFeatures(projectQuery),pointLayer.queryFeatures(projectQuery)]).then(r=>{      
+        const prjcts = [...r[0].features,r[1].features];
+        const polyQuery = {where:"PlantingProjectID IN ("+r[0].features.map(f=>f.attributes[platingProjectIDfield]).join(',')+")",outFields:['*'], returnGeometry:true} as Query
+        const pointQuery = {where:"PlantingProjectID IN ("+r[1].features.map(f=>f.attributes[platingProjectIDfield]).join(',')+")",outFields:['*'], returnGeometry:true} as Query        
+
+        Promise.all([polygonLayer.queryFeatures(polyQuery), pointLayer.queryFeatures(pointQuery)]).then(r=>{
+          const polygonFeatures = r[0].features;
+          const pointFeatures = r[1].features;
+          const polygonCentroidGraphics = polygonFeatures.map(f=>{
+            return new Graphic({attributes:f.attributes,geometry:(f.geometry as Polygon).centroid})
+          })
+
+          const mergedPoints = [...polygonCentroidGraphics, ...pointFeatures];
+          const mergedArrays = createPPIDArray(mergedPoints);
+
+          const dissolvedPoints:Graphic[] = Object.entries(mergedArrays).map(dissolvePoints);
+
+          const pointRenderer = {
+            type:'simple',
+            symbol:{
+              type:'simple-marker',
+              size:8,              
+              outline: { color: [0, 196, 0, 1] },
+              color: [0, 196, 0, .5]
+            } 
+          } as unknown as SimpleRenderer;
+
+          const zoomedOutLayer = new FeatureLayer({
+            source:dissolvedPoints, objectIdField:'OBJECTID',renderer:pointRenderer,
+            maxScale:5000000      
+          });
+
+
+          const middleLayer = new FeatureLayer({
+            source:mergedPoints, objectIdField:'OBJECTID', renderer:pointRenderer,minScale:4999999,maxScale:100000
+          });
+
+          const polyRenderer = {
+            type:'simple',
+            symbol:{
+            type:'simple-fill',
+            outline: {           color: [0, 196, 0, 1] },
+            color: [0, 196, 0, .25]}
+          } as unknown as SimpleRenderer
+          const polygonLayer = new FeatureLayer({source:polygonFeatures,objectIdField:'OBJECTID',renderer:polyRenderer, minScale:99999});
+          pointFeatures.forEach(pf=>pf.popupTemplate= new PopupTemplate({
+            content:generatePopup
+          }));
+          const pointLayer = new FeatureLayer({
+            url:"https://gis-test.arborday.org/server/rest/services/CP_Showcase_MVP_Data_Points/FeatureServer/0",
+            objectIdField:'OBJECTID',
+            renderer:pointRenderer, 
+            minScale:99999,
+            outFields:['*'],
+            popupEnabled:true,
+            popupTemplate:{content:({graphic}:{graphic:Graphic}) => {
+              
+              return JSON.stringify(graphic)
+            }}
+          });
+          map.addMany([zoomedOutLayer,middleLayer,polygonLayer, pointLayer]);
+        } )        
       })
-
-
-
-      // const drawnLayer = new FeatureLayer({
-      //   feat
-      // })
-
-      // map.add(polygonLayer);
-
-      
-
-
 
     }
   }, []);
 
   return <div className="mapDiv" ref={mapDiv}></div>;
 }
+
+const createPPIDArray= (features:Graphic[]): {[key: string]: Graphic[]}=>{
+  const dict = {} as {[key: string]: Graphic[]};
+
+  features.forEach(g=>{
+    dict[g.attributes[platingProjectIDfield]] = dict[g.attributes[platingProjectIDfield]] || [];
+    dict[g.attributes[platingProjectIDfield]].push(g);
+  })
+  return dict;  
+}
+
+const dissolvePoints = ([id,graphics]:[string,Graphic[]],index:number)=>{
+    const point = new Graphic({attributes:{platingProjectIDfield:id,'OBJECTID': index}})
+    point.geometry = averagePointsGeom(graphics);
+    return point;
+}
+
+const averagePointsGeom = (graphics:Graphic[]):Point=>{
+    const x = graphics.reduce((sum,g)=>sum+(g.geometry as Point).x,0)/graphics.length;
+    const y = graphics.reduce((sum,g)=>sum+(g.geometry as Point).y,0)/graphics.length;
+    const spatialReference = graphics[0].geometry.spatialReference;
+    return new Point({spatialReference,x,y});
+}
+
+const generatePopup = (feature:Graphic)=>{
+  console.log(feature);
+  return JSON.stringify(feature);
+}
+
 
 export default ArcGISMap
